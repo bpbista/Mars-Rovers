7c18104ea45a526da7384391d76239ed442892b7
BP Bista <bpbista@macbook-pro-6.local>
2018-05-10 23:49:58 -0400

    Add default values
    The values are added for providing default data if the Internet is not available.

--- Mars Rovers/Controller/ViewController.swift
+++ Mars Rovers/Controller/ViewController.swift
@@ -4,8 +4,8 @@
 //
 //  Created by BP Bista on 5/6/18.
 //  Copyright © 2018 BP Bista. All rights reserved.
+//"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 //
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let scrollWidth = (background.image?.size.width) ?? background.frame.width
         let scrollheight = (background.image?.size.height) ?? background.frame.height
+        imageScroller.contentSize = background.bounds.size
+
+        
         imageScroller.contentSize = CGSize(width: scrollWidth, height: scrollheight)
         fetch()
     }
@@ -33,15 +36,19 @@ class ViewController: UIViewController {
         let urlStr = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
         guard let url = URL(string:urlStr) else {return}
         URLSession.shared.dataTask(with: url) { (data,response ,error) in
+            
             guard let data = data else { return }
             do {
                 let decoder = JSONDecoder()
                 let apodData = try decoder.decode(Apod.self, from: data)
                 DispatchQueue.main.async{
-                    let imgUrl = apodData.url ?? URL(string:"https://api.nasa.gov/images/apod.jpg")
+                    //Reminder DEMO_KEY limits 50 queries per day
+                    let imgUrl = apodData.url ?? URL(string: Values.url.rawValue)
                     self.loadImage(url: imgUrl)
-                    self.imageTitle.text = apodData.title ?? "Astronomy Picture of the Day"
-                    self.imageDescription.text = apodData.explanation
+                    self.imageTitle.text = apodData.title ?? Values.title.rawValue
+                    self.imageDescription.text = apodData.explanation ?? Values.explanation.rawValue
+
+                    print(apodData)
                 }
             } catch let error {
                 print("Error", error)
